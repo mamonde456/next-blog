5c42038d003c096749737b86ddd16674abdeefc7
mamonde456 <mamonde456@gmail.com>
2023-10-18 16:36:16 +0900

    feat: 게시글 작성 기능 변경
    - 임시글 기능 추가
    - 스타일 추가

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,15 +1,41 @@
+import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  gap: 50px;
+`;
+const Form = styled.form`
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+`;
+const TextEditor = styled.textarea`
+  height: 500px;
+`;
+const BtnBox = styled.div`
+  text-align: right;
+`;
+const TextPriveiw = styled.div``;
+
+const BackBtn = styled.button`
+  position: absolute;
+  right: 0;
+  top: 0;
+`;
 export default function Write() {
-  const [data, setData] = useState("");
+  const [text, setText] = useState("");
   const [title, setTitle] = useState("");
+  const router = useRouter();
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    savePost(title, data);
+    savePost(title, text);
   };
   async function savePost(title: string, data: string) {
-    const response = await fetch("/api/test", {
+    const response = await fetch("/api/blog", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -23,22 +49,86 @@ export default function Write() {
 
     return await response.json();
   }
+
+  const handleSave = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    // indexedDB에 임시 저장
+    if (!window) {
+      alert(
+        "Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available."
+      );
+    } else {
+      // window.indexedDB = window.indexedDB;
+      // window.IDBTransaction = window.IDBTransaction;
+      // window.IDBKeyRange = window.IDBKeyRange;
+      const req = window.indexedDB.open("test", 2);
+      req.onerror = function (e) {
+        console.log("error ", e);
+      };
+      req.onupgradeneeded = function (e) {
+        console.log("su", e);
+        // Save the IDBDatabase interface
+        const db = req.result;
+        // Create an objectStore for this database
+        const store = db.createObjectStore("MyObjectStore", { keyPath: "id" });
+        store.createIndex("postIndex", "post");
+      };
+      req.onsuccess = function () {
+        const db = req.result;
+        const tx = db.transaction("MyObjectStore", "readwrite");
+        const store = tx.objectStore("MyObjectStore");
+
+        // add post data
+        store.put({ id: Date.now(), post: { title, text } });
+
+        //query the post data
+        const request = store.openCursor();
+
+        request.onsuccess = function (e: any) {
+          const cursor = e?.target?.result;
+
+          if (cursor) {
+            console.log(
+              `Key: ${cursor.key}, Value: ${JSON.stringify(cursor.value)}`
+            );
+
+            // Move to the next item in the store
+            cursor.continue();
+          } else {
+            console.log("All items have been displayed");
+          }
+
+          db.close();
+        };
+      };
+    }
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <label>title</label>
-      <input
-        type="text"
-        name="title"
-        required
-        onChange={(e) => setTitle(e.currentTarget.value)}
-      ></input>
-      <label>content</label>
-      <textarea
-        name="text"
-        onChange={(e) => setData(e.currentTarget.value)}
-      ></textarea>
-      <input type="button" value="임시저장"></input>
-      <input type="submit" value="업로드"></input>
-    </form>
+    <Wrapper>
+      <Form onSubmit={handleSubmit}>
+        <label>title</label>
+        <input
+          type="text"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.currentTarget.value)}
+        ></input>
+        <label>content</label>
+        <TextEditor
+          name="text"
+          onChange={(e) => setText(e.currentTarget.value)}
+        />
+        <BtnBox>
+          <button onClick={(e) => handleSave(e)}>임시저장</button>
+          <input type="submit" value="업로드"></input>
+        </BtnBox>
+      </Form>
+      <TextPriveiw>
+        <h1>{title}</h1>
+        <ReactMarkdown>{text}</ReactMarkdown>
+      </TextPriveiw>
+      <BackBtn onClick={() => router.push("/")}>{"< 뒤로가기"}</BackBtn>
+    </Wrapper>
   );
 }
