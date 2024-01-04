b77fe8c31316bd2d77e419ac5bbdfdfc4753f251
mamonde456 <mamonde456@gmail.com>
2024-01-04 15:58:12 +0900

    fix: styledComponents 컴파일러 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-};
 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+};
