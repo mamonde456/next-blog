edbdd946d7e374c055ee031b880c010a4d7e3922
mamonde456 <mamonde456@gmail.com>
2024-01-16 16:17:50 +0900

    fix: promise를 async/await으로 변경
    - API URL 조건 분류

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -19,18 +19,23 @@ export const setCurrentUserFollow = (
   });
 };
 
-export const getCurrentUserFollowing = (userId: string, id?: string): any => {
-  let data = [];
+export const getCurrentUserFollowing = async (
+  userId: string,
+  id?: string
+): any => {
   const dbRef = ref(getDatabase());
-  get(child(dbRef, `followUsers/${userId}/${id}`)).then((snapshot) => {
-    console.log(snapshot.exists());
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-      return (data = snapshot.val());
-    } else {
-      console.log("No data available");
-      return (data = []);
-    }
-  });
-  return data;
+  console.log(userId, id);
+  const BASE_URL = `followUsers/`;
+  const USER_ID = `${BASE_URL}${userId}`;
+  const ID = `${BASE_URL}${userId}/${id}`;
+
+  const snapshot = await get(child(dbRef, id ? ID : USER_ID));
+  console.log(snapshot.exists());
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+    return snapshot.val();
+  } else {
+    console.log("No data available");
+    return [];
+  }
 };
