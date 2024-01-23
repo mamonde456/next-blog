b178fd7be69d45233f3849856df0b18afee74944
mamonde456 <mamonde456@gmail.com>
2024-01-23 17:03:32 +0900

    fix: 필요없는 css 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,125 +12,13 @@ const Wrapper = styled.div`
   height: 100%;
   justify-content: center;
   align-items: center;
-  .postItCont {
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
-    align-items: center;
-    width: 100%;
-    .postIt {
-      margin: 20px;
-      background-color: #ffffa5;
-      width: 220px;
-      height: 200px;
-      padding: 10px;
-      padding-top: 25px;
-      box-sizing: border-box;
-      position: relative;
-      top: 300px;
-      z-index: 2;
-      &:hover {
-        transform: rotate(5deg);
-        -webkit-transition: ease 0.5s;
-      }
-      &:before {
-        content: "";
-        position: absolute;
-        display: block;
-        width: 90px;
-        height: 20px;
-        background-color: rgba(255, 232, 0, 0.2);
-        border: solid 1px rgba(255, 232, 0, 0.8);
-        top: -10px;
-        left: 50%;
-        transform: translateX(-50%);
-      }
-      &:nth-child(even) {
-        border-bottom-right-radius: 60px 10px;
-        -webkit-filter: drop-shadow(
-          0px 0px 3px rgba(0, 0, 0, 0.2)
-        ); /* Safari */
-        filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.2));
-      }
-      &:nth-child(odd) {
-        border-bottom-left-radius: 10px 10px;
-        -webkit-filter: drop-shadow(
-          0px 0px 3px rgba(0, 0, 0, 0.2)
-        ); /* Safari */
-        filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.2));
-        /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#ffffa5+0,ffffa5+100&0+8,0+8,1+9,1+9,1+92 */
-        background: -moz-linear-gradient(
-          45deg,
-          rgba(255, 255, 165, 0) 0%,
-          rgba(255, 255, 165, 0) 5%,
-          rgba(255, 255, 165, 1) 9%,
-          rgba(255, 255, 165, 1) 92%,
-          rgba(255, 255, 165, 1) 100%
-        ); /* FF3.6-15 */
-        background: -webkit-linear-gradient(
-          45deg,
-          rgba(255, 255, 165, 0) 0%,
-          rgba(255, 255, 165, 0) 5%,
-          rgba(255, 255, 165, 1) 9%,
-          rgba(255, 255, 165, 1) 92%,
-          rgba(255, 255, 165, 1) 100%
-        ); /* Chrome10-25,Safari5.1-6 */
-        background: linear-gradient(
-          45deg,
-          rgba(255, 255, 165, 0) 0%,
-          rgba(255, 255, 165, 0) 5%,
-          rgba(255, 255, 165, 1) 9%,
-          rgba(255, 255, 165, 1) 92%,
-          rgba(255, 255, 165, 1) 100%
-        ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffa5', endColorstr='#ffffa5',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-        &:after {
-          content: "";
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          width: 0;
-          height: 0;
-          border-top: 30px solid #ffffa5;
-          border-left: 30px solid transparent;
-          -webkit-filter: drop-shadow(
-            0px 0px 3px rgba(0, 0, 0, 0.2)
-          ); /* Safari */
-          filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.2));
-        }
-      }
-
-      .titulo {
-        width: 100%;
-        border-bottom: solid 1px;
-        padding-bottom: 4px;
-        margin-bottom: 8px;
-        font-size: 18px;
-      }
-      .nota {
-        max-height: 125px;
-        height: 100%;
-        font-size: 13px;
-        text-align: justify;
-        padding: 0 21px;
-        display: flex;
-        align-items: center;
-        overflow: hidden;
-        text-overflow: ellipsis;
-
-        li {
-          margin: 5px 0;
-        }
-      }
-    }
-  }
 `;
 
 const WhiteBoard = styled.div`
-  width: 98%;
+  width: 95%;
   height: 70%;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
   gap: 50px;
   border: solid 4px black;
