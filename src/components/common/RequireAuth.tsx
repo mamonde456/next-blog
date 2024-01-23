bd700b7d9b89acf8c51218bc124c9d91073dec2c
mamonde456 <mamonde456@gmail.com>
2024-01-23 17:03:57 +0900

    feat: 리액트 주기와 맞지 않아 유틸 함수로 대체

--- src/components/common/RequireAuth.tsx
+++ src/components/common/RequireAuth.tsx
@@ -1,20 +1,22 @@
 import useAuth from "@/hook/useAuth";
-import { checkAuthentication } from "@/middleware/auth";
+import { checkAuthentication } from "@/utils/auth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { JsxElement } from "typescript";
 
 export default function RequireAuth({ children }: JsxElement) {
-  const isUserAuthenticated = useAuth();
+  const [isAuth, setIsAuth] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
     (async () => {
+      const isUserAuthenticated = await checkAuthentication();
+      setIsAuth(isUserAuthenticated);
       if (!isUserAuthenticated) {
         router.push("/login");
       }
     })();
   }, []);
 
-  return isUserAuthenticated ? children : null;
+  return isAuth ? children : null;
 }
