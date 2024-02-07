dd82a34f21df6d949bc30323fb5e122a1525821b
mamonde456 <mamonde456@gmail.com>
2024-02-07 16:33:25 +0900

    fix: 한국 시간 구하는 로직 수정 - 하루 전으로 저장되는 오류

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,10 @@
 export const getKoreanTime = () => {
   const now = new Date();
-  now.setHours(now.getHours() + 9);
-  return now.toISOString();
+  const koreanTime = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+
+  const year = koreanTime.getUTCFullYear();
+  const month = String(koreanTime.getUTCMonth() + 1).padStart(2, "0");
+  const date = String(koreanTime.getUTCDate()).padStart(2, "0");
+
+  return `${year}-${month}-${date}`;
 };
