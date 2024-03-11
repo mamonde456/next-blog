0ceb7b1536309cd43977378998a9d79c35b970d2
mamonde456 <mamonde456@gmail.com>
2024-03-11 16:03:14 +0900

    feat: 임시저장 페이지 ui 수정
    - 임시글 삭제 버튼 추가
    - 사이드 검색 컴포넌트 추가

--- src/pages/saves/index.tsx
+++ src/pages/saves/index.tsx
@@ -1,29 +1,85 @@
+import MainMenu from "@/components/common/MainMenu";
+import BasicButton from "@/components/ui/button/BasicButton";
 import { useRouter } from "next/router";
 import { Fragment, useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  /* display: flex;
+  height: 100%;
   justify-content: center;
-  padding-top: 150px;
+  align-items: center; */
+  width: 100%;
+  height: 98%;
+  display: flex;
+  background: white;
+  margin-top: 0.8%;
+  border-radius: 10px;
 `;
 
+const SideMenuList = styled.div`
+  flex: 2;
+  width: 300px;
+  height: 100%;
+  /* background: yellow; */
+`;
+
+const Search = styled.div`
+  width: 100%;
+  padding: 10px;
+  height: 10%;
+  display: flex;
+  line-height: 28px;
+  align-items: center;
+  position: relative;
+  border-bottom: solid 1px rgba(0, 0, 0, 0.2);
+  margin-bottom: 20px;
+  svg {
+    position: absolute;
+    left: 1rem;
+    fill: #9e9ea7;
+    width: 1rem;
+    height: 1rem;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  line-height: 28px;
+  padding: 0 1rem;
+  padding-left: 2.5rem;
+  border: 2px solid transparent;
+  border-radius: 8px;
+  outline: none;
+  background-color: #f3f3f4;
+  color: #0d0c22;
+  transition: 0.3s ease;
+  &::placeholder {
+    color: #9e9ea7;
+  }
+  &:focus,
+  &:hover {
+    outline: none;
+    border-color: rgba(76, 84, 234, 0.4);
+    background-color: #fff;
+    box-shadow: 0 0 0 4px rgb(76 84 234 / 10%);
+  }
+`;
 const RemoveBtn = styled.div`
   padding: 10px;
-  border-radius: 10px;
-  border: solid 2px #675555;
-  box-sizing: border-box;
-  /* background: #b89191; */
-  /* color: white; */
+  position: absolute;
+  right: 10px;
+  top: 10px;
 `;
 
 const SaveBox = styled.div`
-  width: 100%;
+  /* width: 100%; */
+  flex: 4;
   /* display: flex; */
   /* gap: 10px; */
   padding: 10px;
+  position: relative;
 `;
 
 const Box = styled.div`
@@ -106,10 +162,13 @@ export default function Saves() {
   };
   return (
     <Wrapper>
-      <RemoveBtn onClick={() => handleRemoveSaves()}>
-        모든 임시글 삭제
-      </RemoveBtn>
+      <MainMenu />
       <SaveBox>
+        <RemoveBtn>
+          <BasicButton type="button" onClick={() => handleRemoveSaves()}>
+            모든 임시글 삭제
+          </BasicButton>
+        </RemoveBtn>
         {postList?.map((el) => {
           return (
             <Box key={el.id}>
@@ -127,6 +186,17 @@ export default function Saves() {
           );
         })}
       </SaveBox>
+      <SideMenuList>
+        {" "}
+        <Search>
+          <svg aria-hidden="true" viewBox="0 0 24 24">
+            <g>
+              <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+            </g>
+          </svg>
+          <Input placeholder="Search" type="search" className="input" />
+        </Search>
+      </SideMenuList>
     </Wrapper>
   );
 }
