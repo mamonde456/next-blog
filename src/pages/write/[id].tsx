786481dbea53e55c964c5483e9816c387ab0a2b1
mamonde456 <mamonde456@gmail.com>
2024-03-11 16:08:42 +0900

    fix: 글작성 로직 수정 및 ui 수정
    - ui 수정
        - input > styled-components로 컴포넌트화
        - basic/submit/back button 컴포넌트 추가
    - text를 decode하여 저장
    - firebase 저장 로직 추가
    - 로그인 유효 시 local/firebase 저장 분리
    - 임시 저장 로직 삭제 및 임시 저장 함수 호출(로직 분리)

--- src/pages/write/[id].tsx
+++ src/pages/write/[id].tsx
@@ -1,76 +1,165 @@
 import { useRouter } from "next/router";
 import { FormEvent, useEffect, useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 import { auth, firestore } from "../../../firebase";
-import RequireAuth from "@/components/common/RequireAuth";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import useAuth from "@/hook/useAuth";
+import { getKoreanTime } from "@/utils/common";
+import { v4 as uuidv4 } from "uuid";
+import { checkAuthentication } from "@/utils/auth";
+import { saveDraftToIndexedDB } from "@/utils/\bblog";
+import { IIndexedDB } from "@/types/blog";
+import BasicButton from "@/components/ui/button/BasicButton";
+import SubmitButton from "@/components/ui/button/SubmitButton";
+import BackButton from "@/components/ui/button/BackButton";
 
 const Wrapper = styled.div`
+  /* display: flex;
   height: 100%;
-  padding: 20px;
-  display: flex;
-  gap: 50px;
-  align-items: center;
   justify-content: center;
-  padding-top: 150px;
+  align-items: center; */
+  width: 100%;
+  height: 98%;
+  display: flex;
+  gap: 10px;
+  background: white;
+  margin-top: 0.8%;
+  border-radius: 10px;
 `;
 const Form = styled.form`
   width: 100%;
   height: 100%;
+  flex: 1;
   display: flex;
   flex-direction: column;
-  padding-right: 40px;
-  border-right: solid 1px black;
+  justify-content: center;
   position: relative;
+  padding: 10px;
   .title {
     height: 50px;
     font-size: 24px;
   }
 `;
+const InputLabel = styled.div`
+  /* color: white; */
+  /* opacity: 0.7; */
+  position: absolute;
+  top: 10px;
+  left: 20px;
+  font-size: 22px;
+  color: rgba(0, 0, 0, 0.4);
+  -webkit-transition: ease 0.2s;
+`;
+const InputBox = styled.div`
+  width: 100%;
+  height: 60px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+  &:focus + input {
+    border: none;
+  }
+  input {
+    width: 100%;
+    height: 100%;
+    font-size: 22px;
+    background: none;
+    text-indent: 10px;
+    border: solid 1px rgba(0, 0, 0, 0.5);
+    border-radius: 5px;
+    &:focus {
+      outline: none;
+      border: solid 1px #36c2597f;
+    }
+    &:focus + ${InputLabel} {
+      top: -8px;
+      background-color: #ffffff;
+      font-size: 14px;
+      color: #36c259;
+    }
+    &:not(:placeholder-shown) + ${InputLabel} {
+      top: -8px;
+      background-color: white;
+      font-size: 14px;
+    }
+  }
+`;
+
 const TextEditor = styled.textarea`
   height: 550px;
+  resize: none;
+  border-radius: 5px;
+  &:focus {
+    outline: none;
+    border: solid 1px #36c2597f;
+  }
 `;
 
 const BtnBox = styled.div`
-  text-align: right;
+  padding: 10px;
+  display: flex;
+  justify-content: right;
+  gap: 10px;
 `;
 const TextPriveiw = styled.div`
-  width: 100%;
-  height: 100%;
-  position: relative;
-  background-image: url("/notebook.jpg");
-  background-size: cover;
+  /* width: 100%; */
+  /* height: 100%; */
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 10px;
+  /* position: relative; */
+  /* background-image: url("/notebook.jpg"); */
+  /* background-size: cover; */
 `;
 const TextContents = styled.div`
   width: 100%;
-  max-width: 370px;
-  max-height: 450px;
+  max-width: 570px;
+  height: 540px;
   overflow-y: scroll;
   word-break: break-all;
-  position: absolute;
+  /* position: absolute;
   top: 100px;
-  left: 100px;
+  left: 100px; */
   line-height: 23px;
+  border-bottom: solid 1px rgba(0, 0, 0, 0.3);
 `;
 const Title = styled.h1`
   width: 100%;
-  max-width: 350px;
-  max-height: 85px;
-  overflow-y: scroll;
+  max-width: 570px;
+  height: 60px;
   padding: 10px;
   word-break: break-all;
   margin-bottom: 20px;
-  position: absolute;
-  top: 10px;
-  left: 110px;
+  overflow-x: auto;
+  white-space: nowrap;
+  /* position: absolute; */
+  /* top: 10px; */
+  /* left: 110px; */
 `;
-
-const BackBtn = styled.button`
+const Description = styled.div`
+  width: 100%;
+  max-width: 570px;
+  height: 60px;
+  padding: 10px;
+  overflow-x: auto;
+  white-space: nowrap;
+  margin-bottom: 20px;
+`;
+const BackBtnContainer = styled.div`
   position: absolute;
-  right: 0;
-  top: 0;
+  right: 10px;
+  bottom: 20px;
 `;
 export default function Write() {
   const [title, setTitle] = useState("");
@@ -79,44 +168,55 @@ export default function Write() {
   const router = useRouter();
   const { action, id: params } = router.query;
   const isLoggedIn = useAuth();
+  // console.log(isLoggedIn);
 
   useEffect(() => {
     setContent("");
     setTitle("");
-    // autoSaveDraft() // 5분마다 임시글 자동 저장
-    checkPostExists();
+    (async () => {
+      const isUserAuthenticated = await checkAuthentication();
+      if (!isUserAuthenticated) {
+        // alert("비회원으로 작성된 글은 로컬에 저장되어 유실될 수 있습니다.");
+      }
+      checkPostExists(isUserAuthenticated);
+      autoSaveDraft(); // 5분마다 임시글 자동 저장
+    })();
   }, []);
 
-  const checkPostExists = async () => {
+  const checkPostExists = async (isLoggedIn: boolean) => {
     if (action === "new") {
       return;
     } else if (action === "edit") {
       // 기존 글 가져오기
-      // getPostById()
-      const response = await fetch(`/api/blog?id=${params}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const [mata, content] = await response.json();
-      console.log(mata, content);
-      setTitle(mata.title);
-      setDescription(mata.description);
-      setContent(content);
+      if (isLoggedIn) {
+        getPostById();
+      } else {
+        const response = await fetch(`/api/posts?id=${params}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const [mata, content] = await response.json();
+        console.log(mata, content);
+        setTitle(mata.title);
+        setDescription(mata.description);
+        setContent(content);
+      }
     } else if (action === "draft") {
       // 임시 글 가져오기
-      // getDraftFromIndexDB()
+      getDraftFromIndexDB();
     }
   };
 
   const getPostById = async () => {
-    const docRef = doc(firestore, "blog", `${params}`);
+    const docRef = doc(firestore, "posts", `${params}`);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       console.log("Document data:", docSnap.data());
       const data = docSnap.data();
-      setContent(data.content);
+      const decodedText = decodeURIComponent(data.content);
+      setContent(decodedText);
       setTitle(data.title);
     } else {
       // docSnap.data() will be undefined in this case
@@ -153,16 +253,24 @@ export default function Write() {
   };
 
   const autoSaveDraft = () => {
+    if (!title && !content) return;
+    const config: IIndexedDB = {
+      id: params as string,
+      title,
+      content,
+      description,
+    };
     const time = 1000 * 60 * 5; // 5분
     const timer = setTimeout(() => {
-      saveDraftToIndexedDB();
+      saveDraftToIndexedDB(config);
     }, time);
     return () => clearTimeout(timer);
   };
 
-  const sendPostToFirebase = async (title: string, content: string) => {
+  const sendPostToFirebase = async () => {
+    const encodedText = encodeURIComponent(content);
+
     const user = auth.currentUser;
-    if (!isLoggedIn) return;
     if (user) {
       const userConfig = {
         displayName: user.displayName,
@@ -170,32 +278,50 @@ export default function Write() {
         photoUrl: user.photoURL,
         uid: user.uid,
       };
-      await setDoc(doc(firestore, "blog", `${params}`), {
-        id: params,
-        title,
-        content,
-        userConfig,
-      });
+      try {
+        await setDoc(doc(firestore, "posts", `${params}`), {
+          id: params,
+          title,
+          created_at: getKoreanTime(),
+          description,
+          content: encodedText,
+          userConfig,
+          meta: {
+            like: 0,
+          },
+        });
+        const docRef = doc(firestore, "posts", `${params}`);
+        await updateDoc(docRef, {
+          update_at: serverTimestamp(),
+        });
+        router.push("/");
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // sendPostToFirebase(title, content);
-    createLocalPostFile(title, content);
+    // const isUserAuthenticated = await checkAuthentication();
+    if (isLoggedIn) {
+      sendPostToFirebase();
+    } else {
+      createLocalPostFile();
+    }
   };
-  async function createLocalPostFile(title: string, data: string) {
-    const user = auth.currentUser;
-    if (!user) return;
+  async function createLocalPostFile() {
+    // const user = auth.currentUser;
+    // if (!user) return;
     const userConfig = {
-      displayName: user.displayName,
-      email: user.email,
-      photoUrl: user.photoURL,
-      uid: user.uid,
+      // displayName: user.displayName,
+      // email: user.email,
+      // photoUrl: user.photoURL,
+      uid: uuidv4(),
     };
     console.log(userConfig);
 
-    const response = await fetch("/api/blog", {
+    const response = await fetch("/api/posts", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -204,7 +330,7 @@ export default function Write() {
         id: params,
         title,
         description,
-        content: data,
+        content,
         userConfig,
       }),
     });
@@ -220,106 +346,66 @@ export default function Write() {
     return await response.json();
   }
 
-  const handleSave = (e: FormEvent<HTMLButtonElement>) => {
+  const handleSave = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    saveDraftToIndexedDB();
+    const config: IIndexedDB = {
+      id: params as string,
+      title,
+      content,
+      description,
+    };
+    saveDraftToIndexedDB(config);
   };
 
-  const saveDraftToIndexedDB = () => {
-    // indexedDB에 임시 저장
-    if (!window) {
-      alert(
-        "Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available."
-      );
-    } else {
-      // window.indexedDB = window.indexedDB;
-      // window.IDBTransaction = window.IDBTransaction;
-      // window.IDBKeyRange = window.IDBKeyRange;
-      const req = window.indexedDB.open("test", 2);
-      req.onerror = function (e) {
-        console.log("error ", e);
-      };
-      req.onupgradeneeded = function (e) {
-        console.log("su", e);
-        // Save the IDBDatabase interface
-        const db = req.result;
-        // Create an objectStore for this database
-        const store = db.createObjectStore("MyObjectStore", { keyPath: "id" });
-        store.createIndex("postIndex", "post");
-      };
-      req.onsuccess = function () {
-        const db = req.result;
-        const tx = db.transaction("MyObjectStore", "readwrite");
-        const store = tx.objectStore("MyObjectStore");
-
-        // add post data
-        store.put({ id: router.query.id, post: { title, text } });
-
-        //query the post data
-        const request = store.openCursor();
-
-        request.onsuccess = function (e: any) {
-          const cursor = e?.target?.result;
-
-          if (cursor) {
-            console.log(
-              `Key: ${cursor.key}, Value: ${JSON.stringify(cursor.value)}`
-            );
-
-            // Move to the next item in the store
-            cursor.continue();
-          } else {
-            console.log("All items have been displayed");
-          }
-
-          db.close();
-        };
-      };
-    }
-  };
   return (
-    <RequireAuth>
-      <Wrapper>
-        <Form onSubmit={handleSubmit}>
-          <label>title</label>
+    <Wrapper>
+      <Form onSubmit={handleSubmit}>
+        <InputBox>
           <input
             type="text"
-            // className="title"
+            name="title"
+            autoComplete="off"
+            placeholder=" "
             required
             defaultValue={title}
             onChange={(e) => setTitle(e.currentTarget.value)}
-          ></input>
-          <label>
-            description
-            <input
-              type="text"
-              // className="description"
-              required
-              defaultValue={description}
-              onChange={(e) => setDescription(e.currentTarget.value)}
-            ></input>
-          </label>
-          <label>content</label>
-          <TextEditor
-            required
-            defaultValue={content}
-            onChange={(e) => setContent(e.currentTarget.value)}
           />
-          <BtnBox>
-            <button type="button" onClick={(e) => handleSave(e)}>
-              임시저장
-            </button>
-            <input type="submit" value="업로드"></input>
-          </BtnBox>
-        </Form>
-        <TextPriveiw>
-          <Title>{title}</Title>
-          <TextContents>
-            <ReactMarkdown>{content}</ReactMarkdown>
-          </TextContents>
-        </TextPriveiw>
-        <BackBtn onClick={() => router.push("/")}>{"< 뒤로가기"}</BackBtn>
-      </Wrapper>
-    </RequireAuth>
+          <InputLabel>title</InputLabel>
+        </InputBox>
+        <InputBox>
+          <input
+            type="text"
+            name="description"
+            placeholder=" "
+            required
+            defaultValue={description}
+            onChange={(e) => setDescription(e.currentTarget.value)}
+          />
+          <InputLabel>description</InputLabel>
+        </InputBox>
+        <label>content</label>
+        <TextEditor
+          required
+          defaultValue={content}
+          onChange={(e) => setContent(e.currentTarget.value)}
+        />
+        <BtnBox>
+          <BasicButton type="button" onClick={(e) => handleSave(e)}>
+            임시저장
+          </BasicButton>
+          <SubmitButton onClick={() => handleSubmit} />
+        </BtnBox>
+      </Form>
+      <TextPriveiw>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+        <TextContents>
+          <ReactMarkdown>{content}</ReactMarkdown>
+        </TextContents>
+      </TextPriveiw>
+      <BackBtnContainer onClick={() => router.push("/")}>
+        <BackButton />
+      </BackBtnContainer>
+    </Wrapper>
   );
 }
