62369d4ad9fe5308d75a4e38503f03fcfc91c6e9
mamonde456 <mamonde456@gmail.com>
2024-03-12 17:17:34 +0900

    feat: guestbooks type 추가 및 확장

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,7 +1,19 @@
+import { Timestamp } from "firebase/firestore";
+
 export interface IUserInfo {
   email: string;
   displayName: string | null;
   photoUrl: string | null;
   uid: string;
   bio: string | null;
+}
+
+export interface IGuestBooks extends IUserInfo {
+  guestbooks: IGuestbook[];
+}
+
+export interface IGuestbook {
+  author: string;
+  message: string;
+  timestamp: Timestamp;
 }
