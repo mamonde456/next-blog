f22a5d3a9ab48b2ae70eeeb0963b5e74cb2bf93f
mamonde456 <mamonde456@gmail.com>
2024-03-15 17:12:13 +0900

    feat: 사용자 정보 로직 추가
    - 세션 스토리지에서 사용자 정보 조회
    - 모든 사용자 정보 조회
    - 현재 로그인한 유저 정보 조회

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,7 +1,16 @@
 import { IUserInfo } from "@/types/users";
 import { db, firestore } from "../../firebase";
 import { set, ref, get, child, getDatabase } from "firebase/database";
-import { doc, setDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  limit,
+  orderBy,
+  setDoc,
+  query,
+  collection,
+  getDocs,
+} from "firebase/firestore";
 
 interface followDataType {
   name: string;
@@ -21,10 +30,7 @@ export const setCurrentUserFollow = (
   });
 };
 
-export const getCurrentUserFollowing = async (
-  userId: string,
-  id?: string
-) => {
+export const getCurrentUserFollowing = async (userId: string, id?: string) => {
   const dbRef = ref(getDatabase());
   console.log(userId, id);
   const BASE_URL = `followUsers/`;
@@ -47,4 +53,38 @@ export const setUserData = async (userInfo: IUserInfo) => {
   await setDoc(doc(firestore, "users", id), {
     ...userInfo,
   });
+};
+
+export const getUserInfoFromSession = () => {
+  const userInfoString = window.sessionStorage.getItem("userInfo");
+  return userInfoString ? JSON.parse(userInfoString) : null;
+};
+
+export const getCurrentUserData = async (currentUserId?: string) => {
+  const docRef = doc(firestore, `users/${currentUserId}`);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    console.log("사용자 데이터가 존재합니다.");
+    return docSnap.data();
+  } else {
+    console.log("사용자 데이터가 존재하지 않습니다.");
+    return;
+  }
+};
+
+export const getAllUserData = async () => {
+  try {
+    const userRef = collection(firestore, "users");
+    const q = query(userRef, orderBy("uid"), limit(10));
+    const querySnapshot = await getDocs(q);
+    const users: IUserInfo[] = [];
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      const user = doc.data() as IUserInfo;
+      users.push(user);
+    });
+    return users;
+  } catch (error) {
+    console.log("get All User Data Error , ", error);
+  }
 };
