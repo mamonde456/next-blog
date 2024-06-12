5bc85d363376999143ceca28f4ec3c7ef47a8d35
mamonde456 <mamonde456@gmail.com>
2024-06-12 16:59:51 +0900

    feat: 프로필 이미지 다운로드 기능 추가

--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -1,4 +1,4 @@
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { auth, firestore, storage } from "../../../../firebase";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { updateProfile } from "firebase/auth";
@@ -10,7 +10,6 @@ import ReactCrop, {
   type Crop,
 } from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
-import ProfileImageCropper from "@/components/users/ProfileImageCropper";
 import MainMenu from "@/components/common/MainMenu";
 import EditButton from "@/components/ui/button/EditButton";
 import BasicButton from "@/components/ui/button/BasicButton";
@@ -20,6 +19,8 @@ import MainPosts from "@/components/blog/MainPosts";
 import { collection, where, query, getDocs } from "firebase/firestore";
 import { IFirebasePost } from "@/types/blog";
 import ItemList from "@/components/ui/ItemList";
+import { getUserInfoFromSession } from "@/utils/user";
+import { IUserInfo } from "@/types/users";
 
 const Wrapper = styled.div`
   /* display: flex;
@@ -39,25 +40,46 @@ const ContentContainer = styled.div`
 const ProfileContainer = styled.div`
   width: 100%;
   height: 200px;
-  background: #eeeeee;
+  background-color: #ffe395;
   padding: 10px;
-  margin-top: 100px;
+  /* padding-top: 100px; */
+  /* margin-top: 100px; */
   /* position: relative;
   top: 100px; */
+  display: flex;
+  gap: 20px;
 `;
-const ProfileImg = styled.div`
+const ProfileImg = styled.div<{ src: string }>`
   width: 170px;
   height: 170px;
-  background: white;
-  border-radius: 100%;
+  background-color: white;
+  background-image: ${(props) =>
+    `url("${props.src ? props.src : "/blank-profile.svg"}")`};
+  /* background-image: url("/blank-profile.svg"); */
+  background-size: cover;
+  background-position: center;
+  /* border-radius: 100%; */
+  border-radius: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative;
-  top: -80px;
+  /* position: relative; */
+  /* top: -80px; */
   border: solid 1px black;
 `;
-const ProfileContent = styled.div``;
+const ProfileContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #3f3f3f;
+  :first-child {
+    font-size: 32px;
+    font-family: Cinzel;
+    color: black;
+  }
+  :nth-child(2) {
+    margin-bottom: 20px;
+  }
+`;
 
 const LinkButton = styled(Link)`
   font-family: monospace;
@@ -105,23 +127,27 @@ const Post = styled.li`
 `;
 
 export default function UserProfile() {
-  const [userInfo, setUserInfo] = useState();
+  const [userInfo, setUserInfo] = useState<IUserInfo>();
   const [postList, setPostList] = useState<IFirebasePost[]>([]);
   const {
     query: { id },
   } = useRouter();
 
   useEffect(() => {
-    const sessionUserInfo =
-      window.sessionStorage.getItem("userInfo") &&
-      JSON.parse(window.sessionStorage.getItem("userInfo") || "");
-    console.log(sessionUserInfo);
+    const sessionUserInfo = getUserInfoFromSession();
     setUserInfo(sessionUserInfo);
-    getQueryPosts();
   }, []);
 
+  useEffect(() => {
+    console.log(userInfo);
+  }, [userInfo]);
+
+  useEffect(() => {
+    getProfileImg();
+    getQueryPosts();
+  }, [id]);
+
   const getQueryPosts = async () => {
-    console.log(id);
     const postsRef = collection(firestore, "posts");
     const q = query(postsRef, where("userConfig.uid", "==", `${id}`));
     const querySnapshot = await getDocs(q);
@@ -132,14 +158,50 @@ export default function UserProfile() {
     });
     setPostList([...posts]);
   };
+
+  const getProfileImg = () => {
+    const storage = getStorage();
+    const userInfo =
+      window.sessionStorage.getItem("userInfo") &&
+      JSON.parse(window.sessionStorage.getItem("userInfo") || "");
+    console.log(userInfo);
+    const imgSrc = `profile-image/${id || userInfo.uid}`;
+    console.log(imgSrc);
+    getDownloadURL(ref(storage, imgSrc))
+      .then((url) => {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = "blob";
+        xhr.onload = (event) => {
+          const blob = xhr.response;
+        };
+        xhr.open("GET", url);
+        xhr.send();
+        console.log(url);
+        setUserInfo((prev) => {
+          if (!prev) return undefined;
+          return {
+            ...prev,
+            photoUrl: url,
+          };
+        });
+      })
+      .catch((error) => {
+        console.log("get image error: ", error);
+      });
+  };
+
   return (
     <Wrapper>
       <MainMenu />
       <ContentContainer>
         <ProfileContainer>
           <LinkButton href={`${id}/setting-profile`}>정보 수정</LinkButton>
-          <ProfileImg></ProfileImg>
-          <ProfileContent></ProfileContent>
+          <ProfileImg src={userInfo?.photoUrl || ""}></ProfileImg>
+          <ProfileContent>
+            <span>{userInfo?.displayName}</span>
+            <span>@{userInfo?.email?.split("@")[0]}</span>
+            <span>{userInfo?.bio}</span>
+          </ProfileContent>
         </ProfileContainer>
         <ItemList itemList={postList}></ItemList>
       </ContentContainer>
