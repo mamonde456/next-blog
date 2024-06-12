5859d3a47cceab20bd543b5703a0eed01d92bc02
mamonde456 <mamonde456@gmail.com>
2024-06-12 16:46:40 +0900

    feat: 북마크 기능 유틸 함수 추가

---
+++ src/utils/features/bookmark.ts
@@ -0,0 +1,33 @@
+import { set, ref, get, child, onValue, remove } from "firebase/database";
+import { db } from "../../../firebase";
+import { IFirebasePost } from "@/types/blog";
+
+export const addPostToBookmark = (
+  userId: string,
+  config: Omit<IFirebasePost, "update_at" | "content">
+) => {
+  set(ref(db, `bookmarks/${userId}/${config.id}`), {
+    ...config,
+  });
+};
+
+export const removePostFromBookmark = (userId: string, postId: string) => {
+  const firebasePosts = listenToBookmarkChanges(userId);
+  const Post = firebasePosts.find((post) => post?.id === postId);
+  if (Post) {
+    remove(ref(db, `bookmarks/${userId}/${postId}`));
+  } else {
+    return console.log("해당 포스트를 찾을 수 없습니다.");
+  }
+};
+
+const listenToBookmarkChanges = (userId: string) => {
+  console.log(userId);
+  let dataArr: IFirebasePost[] = [];
+  const bookmarkRef = ref(db, "bookmarks/" + userId);
+  onValue(bookmarkRef, (snapshot) => {
+    const data = snapshot.val();
+    dataArr.push(data);
+  });
+  return dataArr;
+};
