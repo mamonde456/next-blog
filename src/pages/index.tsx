6d8037a8b2af22f2543820f48dc0441a4be3ba3d
mamonde456 <mamonde456@gmail.com>
2024-06-12 17:02:49 +0900

    fix: 주석 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -165,10 +165,6 @@ export default function Home() {
 
   return (
     <Wrapper>
-      {/* <WhiteBoard>
-        <BlogNavbar setKeyword={getKeyword} />
-        <BlogPostList metaList={metaList} keyword={keyword} />
-      </WhiteBoard> */}
       <MainMenu />
       <MainPosts getPostList={getPostList} />
       <SideMenuList>
@@ -209,31 +205,6 @@ export default function Home() {
             </>
           )}
         </Search>
-        {/* <Bookmark>
-          <BookMarkTitle>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path d="M0 96C0 60.7 28.7 32 64 32H196.1c19.1 0 37.4 7.6 50.9 21.1L289.9 96H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V160c0-8.8-7.2-16-16-16H286.6c-10.6 0-20.8-4.2-28.3-11.7L213.1 87c-4.5-4.5-10.6-7-17-7H64z" />
-            </svg>
-            <span>북마크 리스트</span>
-          </BookMarkTitle>
-          <ul>
-            <li>
-              <BookListTitle>북마크</BookListTitle>
-              <div>
-                <div>북마크</div>
-                <div>북마크</div>
-                <div>북마크</div>
-                <div>북마크</div>
-              </div>
-            </li>
-            <li>
-              <div>북마크</div>
-            </li>
-            <li>
-              <div>북마크</div>
-            </li>
-          </ul>
-        </Bookmark> */}
       </SideMenuList>
     </Wrapper>
   );
