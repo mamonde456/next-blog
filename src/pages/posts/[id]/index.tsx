978341a8cd8f6789cf726f378a761ce7ec63a889
mamonde456 <mamonde456@gmail.com>
2025-05-14 17:00:46 +0900

    feat: 노션 데이터를 mdx로 렌더링

--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -1,28 +1,33 @@
-import path from "path";
-import fs from "fs-extra";
-import matter from "gray-matter";
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 import styled from "styled-components";
-import { auth, db, firestore } from "../../../../firebase";
-import { set, ref, get, child, getDatabase, remove } from "firebase/database";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import useUserInfo from "@/hook/useUserInfo";
-import { getCurrentUserFollowing, setCurrentUserFollow } from "@/utils/user";
-import useAuth from "@/features/auth/hook/useAuth";
-import { useRouter } from "next/router";
-import { deleteDoc, doc, getDoc } from "firebase/firestore";
-import { checkAuthentication } from "@/utils/auth";
-import DeleteButton from "@/components/ui/button/DeleteButton";
-import EditButton from "@/components/ui/button/EditButton";
-import BackButton from "@/components/ui/button/BackButton";
 import MainMenu from "@/shared/components/MainMenu";
-import { IUserInfo } from "@/types/users";
-import { IFirebasePost, IMeta } from "@/types/blog";
-import { formatTimestampToDateStr } from "@/utils/common";
-import { getPostById, removePostByIdFromFirebase } from "@/utils/\bblog";
-import { GetStaticPaths, GetStaticProps } from "next";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import {
+  getMarkdownFromNotionPage,
+  getSlugMap,
+} from "@/features/blog/services/notion";
+import { GetStaticProps } from "next";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeKatex from "rehype-katex";
+import remarkBreaks from "remark-breaks";
+import rehypePrettyCode from "rehype-pretty-code";
+import PageLoader from "@/components/ui/loader/PageLoader";
+import { getNotionPage, getNotionPageBlocks } from "@/features/blog/api/notion";
+import CodeSlot from "@/components/blog/CodeSlot";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Image from "next/image";
+import {
+  getCacheData,
+  replaceCodeFormat,
+  saveJSON,
+} from "@/features/blog/services/notion";
+import { getMdxFile } from "@/features/blog/api/public";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -91,10 +96,16 @@ const UserImg = styled.div<{ src: string }>`
 
 const Content = styled.div`
   width: 100%;
+  height: 100%;
   padding: 30px;
   padding-bottom: 200px;
   background-color: white;
+  img {
+    width: 100%;
+  }
 `;
+
+const NotionComponents = styled.div``;
 
 const BtnContainer = styled.div`
   display: flex;
@@ -103,152 +114,33 @@ const BtnContainer = styled.div`
   padding: 10px 0px;
   gap: 10px;
 `;
-
-export default function Detail({
-  post,
-}: {
-  post: { meta: IMeta; content: string };
-}) {
-  const [isFollow, setIsFollow] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [meta, setMeta] = useState<IMeta>(post?.meta || {});
-  const [posts, setPosts] = useState(post?.content || "");
-  const isLoggedIn = useAuth();
-  const router = useRouter();
-  const { id } = router.query;
-  const userInfoRef = useRef<IUserInfo | null>(null);
-
-  const checkIfAuthorIsFollowed = async (metaData: IMeta | undefined) => {
-    const userSessionInfo = window.sessionStorage.getItem("userInfo") || "";
-    const userInfo = userSessionInfo && JSON.parse(userSessionInfo);
-    const userId = auth.currentUser?.uid || userInfo.uid;
-    userInfoRef.current = userInfo;
-    if (userInfo && metaData) {
-      // 현재 로그인한 유저
-      // 현재 방문한 게시글 작성자
-      const id = meta?.userConfig.uid;
-      const currentFollowUsers = await getCurrentUserFollowing(userId, id);
-      const currentFollowUsersKeys = Object.keys(currentFollowUsers);
-
-      if (currentFollowUsersKeys.length > 0) {
-        setIsFollow(true);
-        setIsLoading(true);
-      } else {
-        setIsFollow(false);
-        setIsLoading(true);
-      }
-    } else return;
-  };
+export default function Detail({ source }: { source: any }) {
   useEffect(() => {
-    if (post) {
-      checkIfAuthorIsFollowed(post?.meta);
-    }
+    console.log(source);
   }, []);
-
-  const handleFollowButtonClick = () => {
-    // 현재 로그인한 유저
-    const user = auth.currentUser;
-    const userInfo =
-      window.sessionStorage.getItem("userInfo") &&
-      JSON.parse(window.sessionStorage.getItem("userInfo") || "");
-    const currentUid = user?.uid || userInfo.uid;
-    const uid = meta?.userConfig.uid || "";
-    const metaObj = {
-      displayName:
-        meta?.userConfig.displayName ||
-        meta?.userConfig.email.split("@")[0] ||
-        "",
-      uid,
-      email: meta?.userConfig.email || "",
-      photoUrl: meta?.userConfig.photoUrl || "",
-    };
-
-    // 팔로우 컬렉션에 추가
-    if (isFollow) {
-      const removeRef = ref(db, `followUsers/${currentUid}/${uid}`);
-      remove(removeRef)
-        .then(() => {
-          console.log("데이터 삭제 완료");
-        })
-        .catch((error) => {
-          console.log("데이터 삭제 오류, ", error);
-        });
-      setIsFollow(false);
-    } else {
-      setIsFollow(true);
-      setCurrentUserFollow(currentUid, metaObj);
-    }
-  };
-
-  const onRemovePost = async (id: string) => {
-    // firebase 문서 삭제
-    const result = await removePostByIdFromFirebase(id);
-    if (result) {
-      alert("문서가 삭제되었습니다.");
-      router.push("/");
-    } else {
-      alert("문서의 삭제에 실패했습니다. 다시 시도해주세요.");
-    }
-
-    // 로컬 문서 삭제
-    // const response = await fetch(`/api/posts?id=${meta?.id}`, {
-    //   method: "DELETE",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // });
-    // if (response.ok) {
-    //   router.push("/");
-    // }
-  };
-
-  const handleEdit = () => {
-    if (id) {
-      router.push({
-        pathname: `/write/${id}`,
-        query: { action: "edit" },
-      });
-    }
-  };
   return (
     <Wrapper>
       <MainMenu />
       <NotebookWrap>
-        <Title>{post?.meta?.title || ""}</Title>
-
-        <MetaData>
-          <UserInfo>
-            <UserImg src={post?.meta?.userConfig?.photoUrl || ""}></UserImg>
-            <span>
-              {post?.meta?.userConfig?.displayName ||
-                post?.meta?.userConfig?.email.split("@")[0]}
-            </span>
-            {post?.meta?.userConfig?.email !== auth?.currentUser?.uid && (
-              <>
-                {post?.meta?.userConfig.uid !== auth.currentUser?.uid && (
-                  <button onClick={handleFollowButtonClick}>
-                    {isFollow ? "following" : "follow"}
-                  </button>
-                )}
-              </>
-            )}
-          </UserInfo>
-          <PostsInfo>
-            {post?.meta?.userConfig?.uid === auth?.currentUser?.uid && (
-              <BtnContainer>
-                <EditButton onClick={handleEdit} />
-                <DeleteButton onClick={() => onRemovePost(post.meta.id)} />
-              </BtnContainer>
-            )}
-            <div>
-              {formatTimestampToDateStr(post?.meta?.created_at || null)}
-            </div>
-          </PostsInfo>
-        </MetaData>
-        <Content style={{ whiteSpace: "pre-line" }}>
-          <Markdown remarkPlugins={[[remarkGfm, { singleTilde: false }]]}>
-            {posts}
-          </Markdown>
+        <Content>
+          {source && (
+            <MDXRemote
+              {...source}
+              components={{
+                img: ({ src, alt }) => (
+                  <Image
+                    src={src || ""}
+                    alt={alt || ""}
+                    width={800}
+                    height={500}
+                    loading="lazy"
+                  />
+                ),
+                CodeSlot,
+              }}
+            />
+            // <MDXRemote {...source} />
+          )}
         </Content>
       </NotebookWrap>
       <SideMenuList />
@@ -257,39 +149,38 @@ export default function Detail({
 }
 
 export const getStaticPaths = async () => {
-  // const file = await fs.readdir(path.join("__post"));
-  // const paths = file.map((el) => ({
-  //   params: { id: el.replace(/\.md$/, "") },
-  // }));
+  const slugMap = await getSlugMap();
 
-  return { paths: [], fallback: "blocking" };
+  return {
+    paths: Object.keys(slugMap).map((slug) => ({ params: { id: slug } })),
+    fallback: "blocking",
+  };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  // const markdownWithMetadata = await fs.readFile(
-  //   path.join(`__post/${params.id}.md`),
-  //   "utf-8"
-  // );
-  // if (markdownWithMetadata) {
-  // const { data, content } = await matter(markdownWithMetadata);
-  // return {
-  //   props: {
-  //     data,
-  //     content,
-  //   },
-  // };
-  // }
-  const { id } = params as Params;
-  const post = await getPostById(id);
-  const sanitizedMeta = {
-    ...post?.meta,
-    created_at: post?.meta.created_at
-      ? formatTimestampToDateStr(post?.meta.created_at)
-      : null,
+  const { id: slug } = params as Params;
+  const slugMap = await getSlugMap();
+  const id = slugMap[slug];
+  // const results = await getNotionPageBlocks(id);
+  // const x = await getMarkdownFormNotionBlocks(results);
+  if (!id) return { notFound: true };
+
+  const mdString = await getMarkdownFromNotionPage(id);
+  // const a = replaceCodeFormat(mdString, id);
+  const mdxResult = await serialize(mdString || "", {
+    mdxOptions: {
+      remarkPlugins: [remarkGfm, remarkBreaks],
+      rehypePlugins: [
+        rehypeSlug,
+        rehypeAutolinkHeadings,
+        [rehypePrettyCode, { theme: "github-light" }],
+      ],
+    },
+  });
+  saveJSON("public/mdx", id + ".mdx", mdxResult);
+
+  return {
+    props: { source: mdxResult },
+    revalidate: 60,
   };
-  const sanitizedPost = {
-    ...post,
-    meta: sanitizedMeta,
-  };
-  return { props: { post: sanitizedPost } };
 };
