c23393f5fbad3730c5ba2085c71295e29bd8c507
mamonde456 <mamonde456@gmail.com>
2025-05-19 16:08:52 +0900

    feat: notion 초기화

--- src/lib/notion/client.ts
+++ src/lib/notion/client.ts
@@ -1,6 +1,6 @@
-import { NOTION_TOKEN } from "@/const";
 import { Client } from "@notionhq/client";
 import { NotionToMarkdown } from "notion-to-md";
+import { NOTION_TOKEN } from "../../../const.ts";
 
 export const notion = new Client({ auth: NOTION_TOKEN });
 export const n2m = new NotionToMarkdown({ notionClient: notion });
