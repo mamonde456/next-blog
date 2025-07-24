1afa3517ba7a36a2630f55bbfd1df7f3689db0f4
mamonde456 <mamonde456@gmail.com>
2025-07-24 14:58:07 +0900

    feat: runSync 동기 처리에서 run 비동기 처리로 변경

--- src/features/blog/components/notion/MDXRenderer.tsx
+++ src/features/blog/components/notion/MDXRenderer.tsx
@@ -1,6 +1,6 @@
 import { run, runSync } from "@mdx-js/mdx";
 import Image from "next/image";
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import * as runtime from "react/jsx-runtime";
 
 const components = {
@@ -10,29 +10,58 @@ const components = {
       alt={alt || ""}
       width={800}
       height={400}
-      priority
       sizes="(max-width: 768px) 100vw, 80vw"
       style={{ width: "100%", height: "auto" }}
+      loading="lazy"
       {...props}
     />
   ),
 };
+
 export default function MDXRenderer({
   compiledCode,
 }: {
   compiledCode: string;
 }) {
-  try {
-    const { default: MDXContent } = runSync(compiledCode, {
-      ...runtime,
-      Fragment: React.Fragment,
-    });
+  const [MDXContent, setMDXContent] = useState<React.ReactElement | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    const renderMDX = async () => {
+      try {
+        const result = await run(compiledCode, {
+          ...runtime,
+          Fragment: React.Fragment,
+        });
 
-    if (MDXContent) {
-      return <MDXContent components={components} />;
-    } else return <div>loading...</div>;
-  } catch (error) {
-    console.error("MDX 실행 에러:", error);
-    return <div>Error loading content</div>;
+        if (!result || typeof result.default !== "function") {
+          throw new Error("지원하지 않는 MDX 데이터입니다.");
+        }
+
+        const MDXComponent = result.default;
+        const renderedContent = React.createElement(MDXComponent, {
+          components,
+        });
+        setMDXContent(renderedContent);
+      } catch (error) {
+        console.error("MDX 실행 에러:", error);
+        setError("Error loading contents");
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (compiledCode) renderMDX();
+  }, [compiledCode]);
+
+  if (loading) return <div>...loading</div>;
+
+  if (error) return <div>{error}</div>;
+
+  if (MDXContent) {
+    return MDXContent;
   }
+
+  return <div>No content available</div>;
 }
