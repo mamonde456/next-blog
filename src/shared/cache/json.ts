ab1cdf5536310bf352261b899eb3b6fd9b01adf7
mamonde456 <mamonde456@gmail.com>
2025-08-06 16:20:26 +0900

    fix: 에러 로직 수정/ return -> throw

--- src/shared/cache/json.ts
+++ src/shared/cache/json.ts
@@ -7,10 +7,13 @@ export const updateJSONFile = <T>(path: string, updater: (data: T) => T) => {
     const updated = updater(safeClone(data));
     const slug = path.split("/").at(-1);
     if (!slug)
-      return new Error("slug 추출 실패: path 경로가 완전하지 않습니다.");
-    return saveFile(path, slug, updated);
+      throw new Error("slug 추출 실패: path 경로가 완전하지 않습니다.");
+
+    const result = saveFile(path, slug, updated);
+    if (result.message !== "success")
+      throw new Error(`[updateJSONFile] 실패: ${path} `);
   } catch (error) {
     console.error(`[updateJSONFile] 실패: ${path} `, error);
-    return { message: "error", error };
+    throw error;
   }
 };
