cefeb0a0e799f422092ac196145a9def318669ae
mamonde456 <mamonde456@gmail.com>
2025-08-07 17:07:47 +0900

    fix: path 파싱 로직 수정

--- src/shared/cache/json.ts
+++ src/shared/cache/json.ts
@@ -1,19 +1,22 @@
 import { getCacheData, saveFile } from "../../features/blog/services/notion";
 import { safeClone } from "../../utils/common";
 
-export const updateJSONFile = <T>(path: string, updater: (data: T) => T) => {
+export const updateJSONFile = <T>(
+  fullPath: string,
+  updater: (data: T) => T
+) => {
   try {
-    const data = getCacheData(path);
+    const data = getCacheData(fullPath);
     const updated = updater(safeClone(data));
-    const slug = path.split("/").at(-1);
+    const slug = fullPath.split("/").at(-1);
+    const path = fullPath.substring(0, fullPath.lastIndexOf("/") + 1);
     if (!slug)
       throw new Error("slug 추출 실패: path 경로가 완전하지 않습니다.");
-
     const result = saveFile(path, slug, updated);
     if (result.message !== "success")
-      throw new Error(`[updateJSONFile] 실패: ${path} `);
+      throw new Error(`[updateJSONFile] 실패: ${fullPath} `);
   } catch (error) {
-    console.error(`[updateJSONFile] 실패: ${path} `, error);
+    console.error(`[updateJSONFile] 실패: ${fullPath} `, error);
     throw error;
   }
 };
