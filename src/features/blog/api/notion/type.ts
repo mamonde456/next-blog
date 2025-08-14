a3c68972fe6271d0bc906592fbc9a25528c913b2
mamonde456 <mamonde456@gmail.com>
2025-08-14 15:31:11 +0900

    feat: CacheMeta 타입을 파일 위치 이동 및 slug map 타입 추가

--- src/features/blog/api/notion/type.ts
+++ src/features/blog/api/notion/type.ts
@@ -86,15 +86,3 @@ export interface NotionPage {
     };
   };
 }
-
-export interface CacheMeta {
-  [id: string]: Meta;
-}
-export interface Meta {
-  title: string;
-  created_time: string;
-  last_edited_time: string;
-  cache_generated_at: string;
-  ttl: number;
-  in_trash: boolean;
-}
--- src/types/cache.ts
+++ src/types/cache.ts
@@ -1,10 +1,15 @@
-export interface MetaDataMap {
-  [index: string]: {
-    title: string;
-    created_time: string;
-    last_edited_time: string;
-    cache_generated_at: string;
-    ttl: number;
-    in_trash: boolean;
-  };
+export interface CacheMeta {
+  [id: string]: Meta;
+}
+export interface Meta {
+  title: string;
+  created_time: string;
+  last_edited_time: string;
+  cache_generated_at: string;
+  ttl: number;
+  in_trash: boolean;
+}
+
+export interface CacheSlugMap {
+  [id: string]: string;
 }
