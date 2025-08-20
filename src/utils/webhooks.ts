36dbbd25c3243802e39e48d0d1bd053131601744
mamonde456 <mamonde456@gmail.com>
2025-08-20 15:39:23 +0900

    feat: findKeyByValue에 예외 케이스 추가
    - slugMap 데이터 없을 시 null 반환

--- src/utils/webhooks.ts
+++ src/utils/webhooks.ts
@@ -1,4 +1,5 @@
 export const findKeyByValue = (slugMap: Record<string, string>, id: string) => {
+  if (!slugMap) return null;
   const entry = Object.entries(slugMap).find(([key, value]) => value === id);
   const key = entry ? entry[0] : null;
   return key;
