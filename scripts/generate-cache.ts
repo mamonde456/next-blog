38e6f9541041058552a99992a581323ce0afe809
mamonde456 <mamonde456@gmail.com>
2025-08-22 15:37:47 +0900

    fix: 웹훅 데이터 JSON 파싱

--- scripts/generate-cache.ts
+++ scripts/generate-cache.ts
@@ -2,23 +2,26 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
 import {
-  handleNotionEvent,
+  NotionWebhooksPayload,
   handleNotionWebhook,
 } from "../src/shared/notion/notion-webhooks.ts";
 import { handleCacheMDXTTL } from "../src/shared/notion/mdx.ts";
+import { parseEnvJson } from "@/utils/webhooks.ts";
 
 const getWebhookData = () => {
-  const webhookDataStr = process.env.WEBHOOK_DATA;
-  if (!webhookDataStr) return console.log("웹훅 데이터가 없습니다.");
-  return JSON.parse(webhookDataStr);
+  const webhookRes = process.env.WEBHOOK;
+  const webhook = parseEnvJson<NotionWebhooksPayload>(webhookRes);
+
+  if (!webhook) throw new Error("웹훅 데이터가 없습니다.");
+
+  return webhook;
 };
 
 async function main() {
   handleCacheMDXTTL(); // TTL 만료인지 확인
   const webhook = getWebhookData();
-
   if (!webhook) return;
-  handleNotionWebhook(webhook, handleNotionEvent);
+  handleNotionWebhook(webhook);
 }
 
 main();
