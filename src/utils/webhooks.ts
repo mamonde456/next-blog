27f833b0f9999deb17153fa8fba031ea3d4c04b2
mamonde456 <mamonde456@gmail.com>
2025-08-22 15:32:56 +0900

    feat: 노션 웹훅 데이터 JSON 파싱 함수 추가

--- src/utils/webhooks.ts
+++ src/utils/webhooks.ts
@@ -4,3 +4,20 @@ export const findKeyByValue = (slugMap: Record<string, string>, id: string) => {
   const key = entry ? entry[0] : null;
   return key;
 };
+
+export const parseEnvJson = <T>(envVar: string | undefined) => {
+  if (!envVar) return null;
+  try {
+    return JSON.parse(envVar) as T;
+  } catch (error) {
+    console.error(
+      `parseEnvJson: 데이터 파싱에 실패했습니다. ${envVar} error: ${error}`
+    );
+    throw new Error(`${error}`);
+  }
+};
+
+export const removeHyphens = (str: string) => {
+  if (!str) return null;
+  return str.replace(/-/g, "");
+};
