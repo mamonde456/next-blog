2e98296d62947e79c43bc0056d1d4eba5cfdf619
mamonde456 <mamonde456@gmail.com>
2026-08-26 17:27:57 +0900

    feat: velog 용 마크다운 파싱 로직 추가

---
+++ content-builder/velog/buildVelogMarkdown.ts
@@ -0,0 +1,10 @@
+import { n2m } from "@/lib/notion/client";
+import { decorate } from "../decorators";
+
+export const buildVelogMarkdown = async (pageId: string) => {
+  const mdBlocks = await n2m.pageToMarkdown(pageId);
+
+  const decorateBlocks = mdBlocks.map(decorate);
+
+  return decorateBlocks.map((block) => block.parent).join("\n\n");
+};
